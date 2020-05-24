44ce09df4b6e04ebb887e5935a1e538052c306b0
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2020-05-24 22:12:58 +0300

    add base main function

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+var secret string
+var at int64
+
+func init() {
+	flag.StringVar(&secret, "secret", "", "Your two-factor secret")
+	flag.Int64Var(&at, "at", 0, "")
+}
+
+func main() {
+	flag.Parse()
+
+	secret = DecodeBase32Secret(secret)
+
+	totp := NewDefaultTOTP(secret)
+	fmt.Printf("secret: %s\n", secret)
+
+	var token string
+
+	if at == 0 {
+		token = totp.Now()
+	} else {
+		token = totp.At(at)
+	}
+
+	fmt.Println(token)
 }
