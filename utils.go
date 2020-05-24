b2b1e007496969ddb6d0d87397c96e5808c9ac7f
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2020-05-24 22:12:32 +0300

    add base32 secret decoding

---
+++ utils.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/base32"
+	"strings"
+)
+
+func DecodeBase32Secret(s string) string {
+	s = strings.ToUpper(s)
+
+	missingPadding := len(s) % 8
+	if missingPadding != 0 {
+		s = s + strings.Repeat("=", 8-missingPadding)
+	}
+	bytes, err := base32.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic("decode secret failed")
+	}
+	return string(bytes)
+}
---
+++ utils_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDecodeBase32Secret(t *testing.T) {
+	for _, c := range []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "empty input",
+			input: "",
+			want:  "",
+		},
+		{
+			name:  "test padding #1",
+			input: "GE",
+			want:  "1",
+		},
+		{
+			name:  "test padding #2",
+			input: "GE=====",
+			want:  "1",
+		},
+		{
+			name:  "test lowercase",
+			input: "ge=====",
+			want:  "1",
+		},
+	} {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			if got := DecodeBase32Secret(c.input); got != c.want {
+				t.Errorf("wrong decoded value for %s input, want: %s != got: %s", c.input, c.want, got)
+			}
+		})
+	}
+}
