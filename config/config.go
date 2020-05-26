9404821209c445e3704628f56d3a02a542cabbcd
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2020-05-26 22:32:53 +0300

    Add config opts, Read/Write. Better tests

--- config/config.go
+++ config/config.go
@@ -12,70 +12,119 @@ import (
 )
 
 const (
-	fileName = "config.ini"
+	defaultConfigName = "config.ini"
 )
 
 type item struct {
-	Name      string
-	Issuer    string
-	Key       string
-	Algorithm string
-	Digits    int
+	Name      string `ini:"-"`
+	Issuer    string `ini:"issuer,omitempty"`
+	Key       string `ini:"secret"`
+	Algorithm string `ini:"algorithm,omitempty"`
+	Digits    int    `ini:"digits,omitempty"`
+}
+
+type Opts struct {
+	path     string
+	filename string
 }
 
 type Config struct {
+	opts  Opts
 	Items []item
 }
 
-func defaultConfigDir() string {
-	return filepath.Join(os.Getenv("HOME"), ".config", "clotp")
-}
-
-// loadConfig parses `f` ini config to Config struct
-func loadConfig(f io.Reader) (*Config, error) {
-	var config Config
-
-	cfg, err := ini.Load(f)
+func (c *Config) Read(r io.Reader) error {
+	cfg, err := ini.Load(r)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	for _, section := range cfg.Sections() {
-		key, err := section.GetKey("secret")
-		if err != nil {
+		if section.Name() == ini.DefaultSection {
 			continue
 		}
 
-		s := item{
-			Name:      section.Name(),
-			Issuer:    section.Key("issuer").String(),
-			Key:       key.String(),
-			Algorithm: section.Key("algorithm").String(),
-			Digits:    section.Key("digits").MustInt(),
+		item := item{Name: section.Name()}
+		if err := section.MapTo(&item); err != nil {
+			return err
 		}
 
-		config.Items = append(config.Items, s)
+		if item.Key == "" {
+			continue
+		}
+
+		c.Items = append(c.Items, item)
 	}
 
-	return &config, nil
+	return nil
+}
+
+func (c Config) Write(w io.WriteCloser) error {
+	cfg := ini.Empty()
+
+	for _, item := range c.Items {
+		section, err := cfg.NewSection(item.Name)
+		if err != nil {
+			return err
+		}
+		err = section.ReflectFrom(&item)
+		if err != nil {
+			return err
+		}
+	}
+
+	if _, err := cfg.WriteTo(w); err != nil {
+		return err
+	}
+
+	if err := w.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewDefaultConfig reads config file if it exist or creates new empty one if doesn't
+func NewDefaultConfig() (*Config, error) {
+	opts := Opts{
+		path:     defaultConfigDir(),
+		filename: defaultConfigName,
+	}
+
+	return NewConfig(opts)
 }
 
 // NewConfig reads config file if it exist or creates new empty one if doesn't
-func NewConfig() (*Config, error) {
-	dir := defaultConfigDir()
-	cfgPath := filepath.Join(dir, fileName)
+func NewConfig(opts Opts) (*Config, error) {
+	if opts.path == "" {
+		opts.path = defaultConfigDir()
+	}
 
-	if !fileExists(cfgPath) {
-		if err := os.Mkdir(dir, 0700); err != nil {
+	if opts.filename == "" {
+		opts.filename = defaultConfigName
+	}
+
+	if !pathExists(opts.path) {
+		if err := os.Mkdir(opts.path, 0700); err != nil {
 			return nil, fmt.Errorf("failed to create directory: %w", err)
 		}
+	}
 
+	cfg := &Config{opts: opts}
+
+	cfgPath := filepath.Join(opts.path, opts.filename)
+
+	if !pathExists(cfgPath) {
 		f, err := os.Create(cfgPath)
 		if err != nil {
 			return nil, fmt.Errorf("failred to create config file: %w", err)
 		}
 
-		return loadConfig(f)
+		if err := cfg.Read(f); err != nil {
+			return nil, err
+		}
+
+		return cfg, nil
 	}
 
 	b, err := ioutil.ReadFile(cfgPath)
@@ -83,13 +132,21 @@ func NewConfig() (*Config, error) {
 		return nil, fmt.Errorf("failied to read config file: %w", err)
 	}
 
-	return loadConfig(bytes.NewBuffer(b))
+	if err := cfg.Read(bytes.NewBuffer(b)); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
 }
 
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
+func pathExists(filename string) bool {
+	_, err := os.Stat(filename)
 	if os.IsNotExist(err) {
 		return false
 	}
-	return !info.IsDir()
+	return true
+}
+
+func defaultConfigDir() string {
+	return filepath.Join(os.Getenv("HOME"), ".config", "clotp")
 }
--- config/config_test.go
+++ config/config_test.go
@@ -2,46 +2,53 @@ package config
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
+
+	"gopkg.in/ini.v1"
 )
 
 var (
-	fullConfig = []byte(`
-	[Name-1]
-	issuer=issuer-1
-	secret=secret-key-1
-	algorithm=sha1
-	digits=6
-	`)
+	fullConfig = []byte(`[Name-1]
+issuer=issuer-1
+secret=secret-key-1
+algorithm=sha1
+digits=6
+`)
 
-	onlySecret = []byte(`
-	[Name-2]
-	secret=secret-key-2
-	`)
+	onlySecret = []byte(`[Name-2]
+secret=secret-key-2
+`)
 
-	noSecret = []byte(`
-	[Name-3]
-	issuer=issuer-3
-	algorithm=sha1
-	digits=6
-	`)
+	noSecret = []byte(`[Name-3]
+issuer=issuer-3
+algorithm=sha1
+digits=6
+`)
 
-	multiple = []byte(`
-	[Name-4]
-	issuer=issuer-4
-	secret=secret-key-4
-	algorithm=sha1
-	digits=6
-	[Name-5]
-	issuer=issuer-5
-	secret=secret-key-5
-	algorithm=sha1
-	digits=6
-	`)
+	multiple = []byte(`[Name-4]
+issuer=issuer-4
+secret=secret-key-4
+algorithm=sha1
+digits=6
+
+[Name-5]
+issuer=issuer-5
+secret=secret-key-5
+algorithm=sha1
+digits=6
+`)
 )
 
-func TestLoadConfig(t *testing.T) {
+func TestConfig_Read(t *testing.T) {
+	ini.PrettyEqual = false
+	ini.PrettyFormat = false
+
 	for _, c := range []struct {
 		name  string
 		input []byte
@@ -77,14 +84,128 @@ func TestLoadConfig(t *testing.T) {
 	} {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
-			got, err := loadConfig(bytes.NewReader(c.input))
-			if err != nil {
+			cfg := &Config{}
+			if err := cfg.Read(bytes.NewReader(c.input)); err != nil {
 				panic(err)
 			}
 
-			if !reflect.DeepEqual(c.want, got) {
-				t.Errorf("wrong config loading, want: %+v != got: %+v", c.want, got)
+			if !reflect.DeepEqual(c.want, cfg) {
+				t.Errorf("wrong config loading, want: %+v != got: %+v", c.want, cfg)
 			}
 		})
+	}
+}
+
+type stubWriteCloser struct {
+	*bytes.Buffer
+	closed bool
+}
+
+func (wc *stubWriteCloser) Close() error {
+	wc.closed = true
+	return nil
+}
+
+func TestConfig_Write(t *testing.T) {
+	ini.PrettyEqual = false
+	ini.PrettyFormat = false
+
+	for _, c := range []struct {
+		name string
+		cfg  *Config
+		want []byte
+	}{
+		{
+			name: "check fields parsing",
+			cfg: &Config{Items: []item{
+				{Name: "Name-1", Issuer: "issuer-1", Key: "secret-key-1", Algorithm: "sha1", Digits: 6},
+			}},
+			want: fullConfig,
+		},
+		{
+			name: "check empty fields",
+			cfg: &Config{Items: []item{
+				{Name: "Name-2", Key: "secret-key-2"}},
+			},
+			want: onlySecret,
+		},
+		{
+			name: "check multiple secrets",
+			cfg: &Config{Items: []item{
+				{Name: "Name-4", Issuer: "issuer-4", Key: "secret-key-4", Algorithm: "sha1", Digits: 6},
+				{Name: "Name-5", Issuer: "issuer-5", Key: "secret-key-5", Algorithm: "sha1", Digits: 6},
+			}},
+			want: multiple,
+		},
+	} {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			wc := &stubWriteCloser{bytes.NewBufferString(""), false}
+			if err := c.cfg.Write(wc); err != nil {
+				panic(err)
+			}
+
+			if !wc.closed {
+				t.Error("config not closed")
+			}
+
+			if got := strings.Trim(wc.String(), "\n"); got != strings.Trim(string(c.want), "\n") {
+				t.Errorf("wrong config writing, want: %s != got: %s", c.want, got)
+			}
+		})
+	}
+}
+
+func TestNewConfig_Init(t *testing.T) {
+	dir, err := ioutil.TempDir("", "prefix")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	got, err := NewConfig(Opts{path: dir})
+	if err != nil {
+		panic(err)
+	}
+
+	want := &Config{opts: Opts{path: dir, filename: defaultConfigName}}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("wrong config, want: %+v != got: %+v", want, got)
+	}
+}
+
+func TestNewConfig_Read(t *testing.T) {
+	dir, err := ioutil.TempDir("", "prefix")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	file, err := ioutil.TempFile(dir, "*"+defaultConfigName)
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(file.Name())
+
+	if _, err := file.Write(multiple); err != nil {
+		panic(err)
+	}
+
+	got, err := NewConfig(Opts{path: dir, filename: filepath.Base(file.Name())})
+	if err != nil {
+		fmt.Println(file.Name())
+		panic(err)
+	}
+
+	want := &Config{
+		Items: []item{
+			{Name: "Name-4", Issuer: "issuer-4", Key: "secret-key-4", Algorithm: "sha1", Digits: 6},
+			{Name: "Name-5", Issuer: "issuer-5", Key: "secret-key-5", Algorithm: "sha1", Digits: 6},
+		},
+		opts: Opts{path: dir, filename: filepath.Base(file.Name())},
+	}
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("wrong config, want: %+v != got: %+v", want, got)
 	}
 }
